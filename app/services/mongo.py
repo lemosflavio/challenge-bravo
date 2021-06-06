464aa8edb367910e5254c424c032bef397e06935
Flavio Lemos <flavio.lemos@nodis.com.br>
2021-06-06 20:09:52 -0300

    Creating mongo service to manage exchange_rate

---
+++ app/services/mongo.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from motor.core import AgnosticCollection
+
+from app.models.exchange_rate import ExchangeRateModel
+
+
+class ExchangeRateService:
+    def __init__(self, exchange_rate_collection: AgnosticCollection):
+        self.__collection = exchange_rate_collection
+
+    async def find_one(self, exchange_from: str, exchange_to: str) -> Optional[ExchangeRateModel]:
+        data = await self.__collection.find_one({
+            'exchange_from': exchange_from,
+            'exchange_to': exchange_to
+        })
+        return ExchangeRateModel(**data) if data else None
+
+    async def insert(self, exchange_rate: ExchangeRateModel):
+        await self.__collection.insert_one(exchange_rate.dict())
+
+    async def update(self, exchange_rate: ExchangeRateModel):
+        search = {
+            'exchange_from': exchange_rate.exchange_from,
+            'exchange_to': exchange_rate.exchange_to
+        }
+        update = {
+            "$set": exchange_rate.dict()
+        }
+        await self.__collection.update_one(search, update)
---
+++ tests/services/test_mongo.py
@@ -0,0 +1,83 @@
+from aiohttp.test_utils import unittest_run_loop
+from asynctest import TestCase, MagicMock, CoroutineMock
+
+from app.models.exchange_rate import ExchangeRateModel
+from app.services.mongo import ExchangeRateService
+
+
+# from freezegun import freeze_time
+
+# @freeze_time("2019-04-02")
+
+class ExchangeRateServiceTest(TestCase):
+
+    def setUp(self):
+        self.collection = MagicMock()
+        self.service = ExchangeRateService(self.collection)
+
+    @unittest_run_loop
+    async def test_find_one(self):
+        exchange_from = 'BRL'
+        exchange_to = 'USD'
+        exchange_tax = 5
+        self.collection.find_one = CoroutineMock(return_value={
+            'exchange_from': exchange_from,
+            'exchange_to': exchange_to,
+            'exchange_tax': exchange_tax,
+        })
+
+        exchange_rate = await self.service.find_one(exchange_from, exchange_to)
+
+        self.assertIsNotNone(exchange_rate)
+        self.assertEqual(exchange_from, exchange_rate.exchange_from)
+        self.assertEqual(exchange_to, exchange_rate.exchange_to)
+        self.assertEqual(exchange_tax, exchange_rate.exchange_tax)
+        self.collection.find_one.assert_awaited_once({
+            'exchange_from': exchange_from,
+            'exchange_to': exchange_to
+        })
+
+    @unittest_run_loop
+    async def test_find_one_returns_none(self):
+        exchange_from = 'BRL'
+        exchange_to = 'USD'
+        self.collection.find_one = CoroutineMock(return_value={})
+
+        exchange_rate = await self.service.find_one(exchange_from, exchange_to)
+
+        self.assertIsNone(exchange_rate)
+        self.collection.find_one.assert_awaited_once({
+            'exchange_from': exchange_from,
+            'exchange_to': exchange_to
+        })
+
+    @unittest_run_loop
+    async def test_insert(self):
+        exchange_rate = ExchangeRateModel(**{
+            'exchange_from': 'BRL',
+            'exchange_to': 'USD',
+            'exchange_tax': 5,
+        })
+        self.collection.insert_one = CoroutineMock()
+
+        await self.service.insert(exchange_rate)
+
+        self.collection.insert_one.assert_awaited_once_with(exchange_rate.dict())
+
+    @unittest_run_loop
+    async def test_update(self):
+        exchange_rate = ExchangeRateModel(**{
+            'exchange_from': 'BRL',
+            'exchange_to': 'USD',
+            'exchange_tax': 5,
+        })
+        self.collection.update_one = CoroutineMock()
+
+        await self.service.update(exchange_rate)
+
+        self.collection.update_one.assert_awaited_once_with({
+            'exchange_from': exchange_rate.exchange_from,
+            'exchange_to': exchange_rate.exchange_to
+        }, {
+            "$set": exchange_rate.dict()
+        })
